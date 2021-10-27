31c4dd52c46dece7cfc087a3e7e2f16b59d828e1
mcc72 <92302152+mcc72@users.noreply.github.com>
2021-10-27 10:59:40 +0200

    test array

--- js/datos.js
+++ js/datos.js
@@ -1,32 +1,29 @@
-export const listaCoders = [
-    
-    {
-        name: "Cecilia",
-    },
-    {
-        name: "Sol",
-    },
-    {
-        name: "Luidmila",
-    },
-    {
-        name: "Mayu",
-    },
-    {
-        name: "Miquel",
-    },
-    {
-        name: "Abde",
-    }
+const listCoders = [
+  {
+    name: "Cecilia",
+  },
+  {
+    name: "Sol",
+  },
+  {
+    name: "Luidmila",
+  },
+  {
+    name: "Mayu",
+  },
+  {
+    name: "Miquel",
+  },
+  {
+    name: "Abde",
+  },
+];
 
-]
-//console.log(listaCoders);
-//export const limbo = []
-let DomListaCoders = document.getElementById("pa-coders");
-function printListCoders(object) {
-    DomListaCoders.innerHTML += `<p>${object.name}</p>`
+let DOMlistCoders = document.querySelector("#pa-coders");
+function printlistCoders(object) {
+  DOMlistCoders.innerHTML += `<p>${object.name}</p>`;
 }
 function loadName() {
-    listaCoders.forEach((object) => printListCoders(object);
+  listCoders.forEach((object) => printlistCoders(object));
 }
 loadName();
--- js/index.js
+++ js/index.js
@@ -1,9 +1,1 @@
 import { listaCoders } from "./datos.js";
-let DomListaCoders = document.getElementById("pa-coders");
-function printListCoders(object) {
-    DomListaCoders.innerHTML += `<p>${object.name}</p>`
-}
-function loadName() {
-    listaCoders.forEach((object) => printListCoders(object);
-}
-loadName();
