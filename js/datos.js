577d2d5bab0ad565dd184fb0deb45176315b78f8
Sol <solcis.bio@gmail.com>
2021-11-03 09:04:37 +0100

    new funtion add coder

--- js/datos.js
+++ js/datos.js
@@ -1,67 +1,62 @@
-const listaCoders = [
-  {
-    nombre: "Ceci",
-  },
-  {
-    nombre: "Miquel",
-  },
-  {
-    nombre: "Sol",
-  },
-  {
-    nombre: "Mayu",
-  },
-  {
-    nombre: "Ludmila",
-  },
-  {
-    nombre: "Abde",
-  },
-];
+let listaVivos = ["Ceci", "Abde", "Sol", "Mayu", "Miquel", "Ludmila"];
+let listaMuertos = [];
 
-function printListaCoders() {
-  let listaCoder = document.querySelector(".pac-coders");
-  listaCoder.innerHTML = " ";
+let ulVivos = document.querySelector(".pac-vivos");
+let ulMuertos = document.querySelector(".pac-muertos");
+let botonMatar = document.querySelector("#boton_inicio");
+let botonReset = document.querySelector("#boton_reiniciar");
+let botonAdd = document.querySelector("#add_coder");
+let inputNuevoCoder = document.querySelector("#newCoder");
 
-  listaCoders.forEach(function (object) {
-    listaCoder.innerHTML += `<li>${object.nombre}</li>`;
-  });
-}
-printListaCoders();
-
-let deathCoders = [];
-
-function printMuertos(coderstring) {
-  let listaMuerto = document.querySelector("#pacoders_muertos");
-
+function imprimirLista(lista, ul) {
   let html = "";
-  
-  deathCoders.forEach((coder) => {
+
+  lista.forEach((coder) => {
     html += `<li>${coder}</li>`;
-  })
- 
-  listaMuerto.innerHTML = html;
+  });
+
+  ul.innerHTML = html;
 }
 
+function actualizarListas() {
+  imprimirLista(listaVivos, ulVivos);
+  imprimirLista(listaMuertos, ulMuertos);
+}
 
+function indexAlAzar(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function killCoder() {
-  let randomKill = Math.floor(Math.random() * listaCoders.length);
-  if (listaCoders.length > 0) {
-    let programadorMuerto = listaCoders.splice(randomKill, 1);
-    let stringCoder = programadorMuerto.pop().nombre;
-    deathCoders.push(stringCoder);
-    printListaCoders();
-    printMuertos(stringCoder);
+  let randomKill = indexAlAzar(listaVivos.length);
+
+  if (listaVivos.length > 0) {
+    let programadorMuerto = listaVivos[randomKill];
+
+    listaVivos = listaVivos.filter((_, i) => {
+      return i !== randomKill;
+    });
+    
+    listaMuertos.push(programadorMuerto);
+
+    actualizarListas();
   }
 }
 
-let boton = document.querySelector("#boton_inicio");
-boton.addEventListener("click", killCoder);
-
-
-let reset = document.getElementById("boton_reiniciar");
-
-reset.addEventListener("click", (_) => {
+function reset() {
   location.reload();
-});
+}
+
+function addBoton() {
+  if (inputNuevoCoder.value) {
+    listaVivos.push(inputNuevoCoder.value);
+    inputNuevoCoder.value = "";
+    actualizarListas();
+  }
+}
+
+botonMatar.addEventListener("click", killCoder);
+botonReset.addEventListener("click", reset);
+botonAdd.addEventListener("click", addBoton);
+
+actualizarListas();
