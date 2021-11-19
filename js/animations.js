8bc876b78b2d4074b7cb5c1fef2c6792c8df55a3
Sol <solcis.bio@gmail.com>
2021-11-19 09:10:28 +0100

    tests

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,12 @@
+boton_inicio.onclick = function () {
+  let start = Date.now();
 
-  boton_inicio.onclick = function() {
-    let start = Date.now();
+  let timer = setInterval(function () {
+    let timePassed = Date.now() - start;
 
-    let timer = setInterval(function() {
-      let timePassed = Date.now() - start;
+    Pcman.style.left = timePassed / 3.6 + "px";
+    Gosht.style.left = timePassed / 5 + "px";
 
-      Pcman.style.left = timePassed / 3.6 + 'px';
-      Gosht.style.left = timePassed / 5 + 'px';
-
-      if (timePassed > 1600) clearInterval(timer);
-
-    }, 20);
-  }
+    if (timePassed > 2000) clearInterval(timer);
+  }, 20);
+};
